a99b1519667d88507ee79a23e2a5ff5ba8277ca4
laima7110 <laima.v.71101355@rambler.ru>
2026-06-28 14:00:20 +0000

    Site updated: 2026-06-28 14:00:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/28/hello-world/","2026/06/28/huggingface-spaces-deploy/"];function toRandomPost(){
+var posts=["2026/06/28/huggingface-spaces-deploy/","2026/06/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
